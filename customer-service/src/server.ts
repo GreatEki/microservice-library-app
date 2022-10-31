8b4ec183b291985c394ab63bdcd2b45667bce744
GreatEki <ekeneidiagbor@gmail.com>
2022-10-31 17:35:34 +0100

    chore: initial customer microservice setup

--- customer-service/src/server.ts
+++ customer-service/src/server.ts
@@ -1,1 +1,9 @@
 import express from "express";
+
+const app = express();
+
+const PORT = process.env || 7001;
+
+app.listen(PORT, () =>
+  console.log(`Customer microservice running on PORT ${PORT}`)
+);
