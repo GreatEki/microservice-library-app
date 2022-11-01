cd355d8baefc4aaff0cf61a0eb03899bc638c293
GreatEki <ekeneidiagbor@gmail.com>
2022-11-01 18:22:25 +0100

    feat: direct service to service call

--- order-service/src/controller/order.ts
+++ order-service/src/controller/order.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { Order } from "../model";
+import axios from "axios";
 
 export const createOrder = async (req: Request, res: Response) => {
   try {
@@ -29,12 +30,32 @@ export const getOrders = async (req: Request, res: Response) => {
   try {
     const orders = await Order.find({}).sort({ $natural: -1 });
 
+    const processedOrders = await Promise.all(
+      orders.map(async (order) => {
+        const theCust = await axios.get(
+          `${process.env.CUSTOMER_MICROSERVICE_DOMAIN}/customer/${order.customer}`
+        );
+
+        const theBook = await axios.get(
+          `${process.env.BOOK_MICROSERVICE_DOMAN}/book/${order.book}`
+        );
+
+        return {
+          _id: order._id,
+          customer: theCust.data.data,
+          book: theBook.data.data,
+          orderDate: order.orderDate,
+          deliveryDate: order.deliveryDate,
+        };
+      })
+    );
+
     return res.status(200).json({
       success: true,
       status: "OK",
       statusCode: 200,
       message: "Orders returned",
-      data: orders,
+      data: processedOrders,
     });
   } catch (err) {
     return res.status(err.statusCode || 500).json({
